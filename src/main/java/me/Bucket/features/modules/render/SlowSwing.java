1305750cdb642716638c20ec3f50055765b52331
Femey <113284298+Femey@users.noreply.github.com>
2022-10-26 00:53:03 +1300

    Add files via upload

---
+++ src/main/java/me/Bucket/features/modules/render/SlowSwing.java
@@ -0,0 +1,24 @@
+package me.Bucket.features.modules.test;
+
+import me.Bucket.features.modules.*;
+import me.Bucket.features.setting.*;
+
+public class SlowSwing extends Module
+{
+    public static SlowSwing instance;
+    public Setting<Integer> speed = this.register(new Setting("Speed", 6, 1, 50));
+    public boolean isPressed;
+
+    public SlowSwing() {
+        super("SwingSpeed", "", Module.Category.RENDER, true, false, false);
+        this.isPressed = false;
+        SlowSwing.instance = this;
+    }
+
+    public static SlowSwing getInstance() {
+        if (instance == null) {
+            instance = new SlowSwing();
+        }
+        return instance;
+    }
+}
